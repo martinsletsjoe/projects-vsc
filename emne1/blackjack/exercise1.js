48e3cb235bfd216b55b8987ecaa4543e1e7166cb
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-09-21 15:48:10 +0200

    ïnnlevering update

--- emne1/blackjack/exercise1.js
+++ emne1/blackjack/exercise1.js
@@ -1,24 +1,3 @@
-// let cards = ['Hjerter Ess', ' Hjerter Konge', 'Hjerter Dame', 'Hjerter Knekt', 'Hjerter 10', 'Hjerter 9', 'Hjerter 8', 'Hjerter 7', 'Hjerter 6', 'Hjerter 5', 'Hjerter 4', 'Hjerter 3', 'Hjerter 2',
-//             'Spar Ess', 'Spar Konge', 'Spar Dame', 'Spar Knekt', 'Spar 10', 'Spar 9', 'Spar 8', 'Spar 7', 'Spar 6', 'Spar 5', 'Spar 4', 'Spar 3', 'Spar 2',
-//             'Ruter Ess', 'Ruter Konge', 'Ruter Dame', 'Ruter Knekt', 'Ruter 10', 'Ruter 9', 'Ruter 8', 'Ruter 7', 'Ruter 6', 'Ruter 5', 'Ruter 4', 'Ruter 3', 'Ruter 2',
-//             'Kløver Ess', 'Kløver konge', 'Kløver Dame', 'Kløver Knekt', 'Kløver 10', 'Kløver 9', 'Kløver 8', 'Kløver 7', 'Kløver 6', 'Kløver 5', 'Kløver 4', 'Kløver 3', 'Kløver 2']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function countBlackJackPoints(cards) {
     let points = 0;
     let aces = 0;
