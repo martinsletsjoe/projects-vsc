59e95c6d6045defaf6f35c6116e0b64db653e088
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-10-05 14:56:09 +0200

    klasedrit

---
+++ objekter/jujutsu/model.js
@@ -0,0 +1,38 @@
+const model = {
+    app: {
+        currentPage: 'currentPage'
+    },
+    inputs: {
+        mainPage: {
+            userId: ['marie',],
+            belt: ['gult',],
+            progression: 40,
+            training: {
+                partA: {
+                    guard: '',
+                    forflyttning: '',
+                    fallteknikk: '',
+                    kaste: '',
+                },
+                partB: {
+                    forfra: '',
+                    siden: '',
+                    bakfra: '',
+                    bakketeknikk: '',
+                },
+                partC: {
+                    forsvarUnderPress: '',
+                },
+            }
+            
+        },
+        register: {
+             userName: '',
+             password: '',
+        },
+        login: {
+            userName: '',
+            password: '',
+        }
+    }
+}
