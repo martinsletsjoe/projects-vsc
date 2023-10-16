014216fb4585f5f212cb556d223f5f0eeec08b9a
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-10-16 10:19:23 +0200

    små fiks

--- objekter/jujutsu/js/model.js
+++ objekter/jujutsu/js/model.js
@@ -8,32 +8,13 @@ const model = {
             loggedIn: false,
 
         },
-
-        registrationPage: {
-            id: 2, 
-            date: '2023-09-25',
-            name: '',
-            email: '',
-            password: '',
-
-        },
-        loginPage: {
-            id: 3,
-            email: '',
-            password: '',
-
-        },
         userPage: {
             id: 4 ,
             myGrade: "Gult belte",
             title: null,
             description: null,
         },
-        pensumPage: {
-            id: 5 ,
-            title: null,
-            description: null,
-        },
+
     },
 
     // data
