c07bc0119d86acd60785a0c94b355988f519ef55
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-10-24 20:39:01 +0200

    stuff

---
+++ emne2/asd/js/model.js
@@ -0,0 +1,128 @@
+const model = {
+    southAmericanCountries: [
+        {
+            name: 'Argentina',
+            region: 'Sør-Amerika',
+            population: 45195777
+        },
+        {
+            name: 'Bolivia',
+            region: 'Sør-Amerika',
+            population: 11307682
+        },
+        {
+            name: 'Brasil',
+            region: 'Sør-Amerika',
+            population: 212559417
+        },
+        {
+            name: 'Chile',
+            region: 'Sør-Amerika',
+            population: 19107216
+        },
+        {
+            name: 'Colombia',
+            region: 'Sør-Amerika',
+            population: 50882891
+        },
+        {
+            name: 'Ecuador',
+            region: 'Sør-Amerika',
+            population: 17587273
+        },
+        {
+            name: 'Guyana',
+            region: 'Sør-Amerika',
+            population: 782766
+        },
+        {
+            name: 'Paraguay',
+            region: 'Sør-Amerika',
+            population: 7132532
+        },
+        {
+            name: 'Peru',
+            region: 'Sør-Amerika',
+            population: 32933835
+        },
+        {
+            name: 'Surinam',
+            region: 'Sør-Amerika',
+            population: 586632
+        },
+        {
+            name: 'Uruguay',
+            region: 'Sør-Amerika',
+            population: 3473730
+        },
+        {
+            name: 'Venezuela',
+            region: 'Sør-Amerika',
+            population: 28435943
+        }
+    ],
+    
+    southAmericanBorders: [
+        {
+            countryA: 'Argentina',
+            countryB: 'Bolivia'
+        },
+        {
+            countryA: 'Argentina',
+            countryB: 'Brasil'
+        },
+        {
+            countryA: 'Argentina',
+            countryB: 'Chile'
+        },
+        {
+            countryA: 'Bolivia',
+            countryB: 'Brasil'
+        },
+        {
+            countryA: 'Bolivia',
+            countryB: 'Chile'
+        },
+        {
+            countryA: 'Bolivia',
+            countryB: 'Paraguay'
+        },
+        {
+            countryA: 'Bolivia',
+            countryB: 'Peru'
+        },
+        {
+            countryA: 'Brasil',
+            countryB: 'Colombia'
+        },
+        {
+            countryA: 'Brasil',
+            countryB: 'Guyana'
+        },
+        {
+            countryA: 'Brasil',
+            countryB: 'Paraguay'
+        },
+        {
+            countryA: 'Brasil',
+            countryB: 'Peru'
+        },
+        {
+            countryA: 'Brasil',
+            countryB: 'Surinam'
+        },
+        {
+            countryA: 'Brasil',
+            countryB: 'Venezuela'
+        },
+        {
+            countryA: 'Chile',
+            countryB: 'Peru'
+        },
+        {
+            countryA: 'Colombia',
+            countryB: 'Venezuela'
+        }
+        // Legg til flere grenser etter behov...
+    ],
+}
