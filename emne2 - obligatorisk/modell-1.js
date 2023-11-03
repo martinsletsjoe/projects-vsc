cdba415c3b7949d02c16db3b0e93fe55d96546a5
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-03 14:29:16 +0100

    asd

--- emne2 - obligatorisk/modell-1.js
+++ emne2 - obligatorisk/modell-1.js
@@ -1,3 +1,13 @@
 const model = {
+    player: [
+        { id: 0, name: 'per', },
+        { id: 1, name: 'pål', },
+        { id: 2, name: 'espen', },
+    ],
+    totalSum: [
+        { id: 0, playerId: 0, sum: 0, },
+        { id: 1, playerId: 1, sum: 0, },
+        { id: 2, playerId: 2, sum: 0, },
+    ],
 
 }
