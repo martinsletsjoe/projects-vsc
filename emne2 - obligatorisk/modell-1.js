92ad4c308db61362674f84a0e4426faa9162a5de
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-11-06 14:55:50 +0100

    oblig emne2

--- emne2 - obligatorisk/modell-1.js
+++ emne2 - obligatorisk/modell-1.js
@@ -1,13 +1,29 @@
 const model = {
-    player: [
-        { id: 0, name: 'per', },
-        { id: 1, name: 'pål', },
-        { id: 2, name: 'espen', },
+    players: [
+        { name: "Per", score: [] },
+        { name: "Pål", score: [] },
+        { name: "Espen", score: [] },
     ],
-    totalSum: [
-        { id: 0, playerId: 0, sum: 0, },
-        { id: 1, playerId: 1, sum: 0, },
-        { id: 2, playerId: 2, sum: 0, },
-    ],
-
-}
+    categories: [
+        //del 1
+        { name: "Enere", scores: [4, null, null], part: 1, },
+        { name: "Toere", scores: [null, null, null], part: 1, },
+        { name: "Treere", scores: [null, null, null], part: 1, },
+        { name: "Firere", scores: [null, null, 20], part: 1, },
+        { name: "Femmere", scores: [null, 15, 25], part: 1, },
+        { name: "Seksere", scores: [null, null, 30], part: 1, },
+        { name: "Sum", scores: [4, 15, 75], part: 1, },
+        //del 2
+        { name: "Bonus", scores: [null, null, 50], part: 2, },
+        { name: "1 par", scores: [12, null, null], part: 2, },
+        { name: "2 par", scores: [null, null, null], part: 2, },
+        { name: "3 like", scores: [null, null, null], part: 2, },
+        { name: "4 like", scores: [null, null, null], part: 2, },
+        { name: "Liten straight", scores: [null, 15, null], part: 2, },
+        { name: "Stor straight", scores: [null, null, null], part: 2, },
+        { name: "Hus", scores: [null, 27, null], part: 2, },
+        { name: "Sjanse", scores: [null, null, null], part: 2, },
+        { name: "Yatzy", scores: [50, null, null], part: 2, },
+        { name: "Totalsum", scores: [66, 57, 125], part: 2, }
+    ]
+};
